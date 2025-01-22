3ae3bc50b20b6dfdd15c1ddc4e05016d77670b73
Olga P. <zjkz14@gmail.com>
2025-01-22 22:28:52 +0300

    выполнение заданий по ДЗ -3

--- hw-2.js
+++ hw-2.js
@@ -26,4 +26,52 @@ if (k > 100 || l > 100) {
     console.log("Неверно");
 }
 //Задание 4//
+let m = '2';
+let n = '3';
+alert(Number(m) + Number(n)); 
+
 //Задание 5//
+let monthNumber = 12;
+if(monthNumber < 1 || monthNumber > 12) {
+    console.log("Номер месяца должен быть от 1 до 12.");
+} else {
+    switch (monthNumber) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Зима");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Весна");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Лето");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("Осень");
+            break;
+    }
+}
+
+//Доп.задание 1//
+let usertInput = prompt("Пожалуйста, введите любое число: ");
+let number = Number(usertInput);
+if (isNaN(number)) {
+    alert("Вы ввели не число!");
+} else {
+    if (number % 2 === 0) {
+        alert("Число четное");
+    } else {
+        alert("Число нечетное");
+    }
+}
+
+//Доп.задание 2//
+
+//Доп.задание 3//
