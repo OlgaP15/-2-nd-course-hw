dab558cd3d1f3079468e98f9b0d5317f73ac7431
Olga P. <zjkz14@gmail.com>
2025-01-22 21:36:39 +0300

    верстка js файла, зад.1, 2, 3

--- hw-2.js
+++ hw-2.js
@@ -4,12 +4,26 @@ let userInput = prompt("Введите пароль");
 
 if (userInput === password) {
     alert("Пароль введен верно");
-}else {
+} else {
     alert("Пароль введен неправильно");
 }
 
 //Задание 2//
+let i = 2;
+if (i > 0 && i <10) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
 
 //Задание 3//
+let k = 120;
+let l = 50;
+
+if (k > 100 || l > 100) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
 //Задание 4//
 //Задание 5//
