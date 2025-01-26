c1cbbe5ab9ddbbfd24788ac875a118e40dc4701f
Olga P. <zjkz14@gmail.com>
2025-01-26 18:58:53 +0300

    исправление недочетов

--- hw-3.js
+++ hw-3.js
@@ -1,19 +1,19 @@
-    //Задание 1
-for (let z = 0; z < 2; z++) {
+    //Задание 3.1
+for (let z = 0; z <= 2; z++) {
     console.log("Привет");
 }
 
-    //Задание 2
+    //Задание 3.2
 for (let g =1; g <= 5; g++) {
     console.log(g);
 }
 
-    //Задание 3
+    //Задание 3.3
 for (let h =7; h <= 22; h++) {
     console.log(h);
 }
 
-    //Задание 4
+    //Задание 3.4
 let obj = {
     "Коля": '200',
     "Вася": '300',
@@ -23,35 +23,35 @@ for (let name in obj) {
     console.log(`${name} - зарплата ${obj[name]} долларов`);
 }
 
-    //Задание 5
-let n =1000;
-let num = 0;
-while (n >= 50) {
-    n /= 2;
-    num++;
+    //Задание 3.5
+let q =1000;
+let numX = 0;
+while (q >= 50) {
+    q /= 2;
+    numX++;
 }
-console.log("Полученное число: ", n);
-console.log("Количество итераций: ", num);
+console.log("Полученное число: ", q);
+console.log("Количество итераций: ", numX);
 
-    //Задание 6
-let (firstFriday = 5);
+    //Задание 3.6
+let firstFriday = 5;
 for (let day = 1; day <= 31; day++) {
     if (day === firstFriday || (day - firstFriday) % 7 === 0) {
         console.log(`Сегодня пятница, ${day}-e число. Необходимо подготовить отчет.`);
     }
 }
 
-       //Доп. Задание 1
-let k = 100;
+    //Доп. Задание 3.1
+let u = 100;
 let iterations = 0;
-while (k >= 0) {
-    k -= 7;
+while (u >= 0) {
+    u -= 7;
     iterations++;
 }
-console.log("Полученное число: ", k);
+console.log("Полученное число: ", u);
 console.log("Количество итераций: ", iterations);
 
-       //Доп. Задание 2
+    //Доп. Задание 3.2
 let months = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
@@ -60,7 +60,7 @@ for (let x = 0; x < months.length; x++) {
     console.log(`${x + 1}. ${months[x]}`);
 }
 
-       //Доп. Задание 3
+    //Доп. Задание 3.3
 let book = {
     title: "1984",
     author: "George Orwell",
@@ -71,7 +71,7 @@ for (let key in book) {
     console.log(`${key}: ${book[key]}`);
 }
 
-       //Доп. Задание 4
+    //Доп. Задание 3.4
 let numbers = [];
 for (let y = 0; y < 10; y++) {
     numbers.push(Math.floor(Math.random() * 100) + 1);
