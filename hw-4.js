5a94be464cc61e862b578514b70b99b4608948c9
Olga P. <zjkz14@gmail.com>
2025-02-01 15:57:13 +0300

    выполнение заданий: 3, 4, 5, 6, 7

--- hw-4.js
+++ hw-4.js
@@ -77,12 +77,96 @@ console.log(checkEvenOrOdd(11));
 console.log(checkEvenOrOdd(14));
 
           //Задание 4.3
+function printSquare(number) {
+    const square = number * number;
+    console.log(`Квадрат числа ${number} равен ${square}`);
+}
+        
+function getSquare(number) {
+    return number * number;
+}
+
+printSquare(5);
+
+const squareValue = getSquare(6);
+console.log(`Квадрат числа 6 равен ${squareValue}`);
 
           //Задание 4.4
+function checkAge() {
+    const age = prompt("Сколько вам лет?");
+        
+    const ageNumber = parseInt(age);
+        
+    if (isNaN(ageNumber)) {
+        alert("Вы ввели неправильное значение");
+        } else if (ageNumber < 0) {
+            alert("Вы ввели неправильное значение");
+        } else if (ageNumber >= 0 && ageNumber <= 12) {
+            alert("Привет, друг!");
+        } else {
+            alert("Добро пожаловать!");
+            }
+}
+checkAge();
 
           //Задание 4.5
+function multiplyNumbers(a, b) {
+    const numA = parseInt(a);
+    const numB = parseInt(b);
+        
+    if (isNaN(numA) || isNaN(numB)) {
+        return 'Одно или оба значения не являются числом';
+    } else {
+        return numA * numB;
+    }
+}
+const cubeResult = multiplyNumbers(5, 3);
+
+console.log(cubeResult); 
 
           //Задание 4.6
+function calculateCube() {
+    const input = prompt("Введите число:");
+        
+    const number = parseFloat(input);
+        
+    if (isNaN(number)) {
+        return 'Переданный параметр не является числом';
+    } else {
+        const cube = number ** 3; 
+        return `${number} в кубе равняется ${cube}`;
+    }
+}
+console.log(calculateCube());
+
+for (let ai = 0; ai <= 10; ai++) {
+    console.log(calculateCube(ai));
+}
 
           //Задание 4.7
+const circle1 = {
+    radius: 5,
+        
+    getArea() {
+        return Math.PI * this.radius ** 2;
+    },
+    getPerimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+};
+        
+const circle2 = {
+    radius: 10,
 
+    getArea() {
+        return Math.PI * this.radius ** 2;
+    },
+    getPerimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+};
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+
+console.log(circle2.getArea()); 
+console.log(circle2.getPerimeter()); 
